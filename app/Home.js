6b346a197a3fbea3960f44609d8982b8b47aedd8
PVega17102 <is.paulvega.17102@gmail.com>
2023-10-25 12:14:43 -0600

    Navigation

--- app/Home.js
+++ app/Home.js
@@ -2,19 +2,19 @@ import { Button, Image, Text, View } from 'react-native';
 
 import styles from './styles/start.module.css'
 
-const Start = ({ navigation }) => {
+const Home = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Image style={styles.logo} source={require('./img/netShield.png')}/>
-      <Text style={styles.title}>Net Shield</Text>
+      {/* <Image style={styles.logo} source={require('./img/netShield.png')}/> */}
+      <Text style={styles.title}>Home</Text>
       <View style={styles.content}>
         <Text style={styles.description}>Tu entrada a la ciberseguridad. Aprende y protege datos con una experiencia flexible para principiantes y profesionales.</Text>
       </View>
       <View style={styles.button}>
-        <Button title='COMENZAR' color={'#5D92B0'} onPress={() => navigation.navigate('Home')}/>
+        <Button title='COMENZAR' color={'#5D92B0'} onPress={() => navigation.navigate('Start')}/>
       </View>
     </View>
   )
 }
 
-export default Start
+export default Home
