7431b46fbc15728420c5ac5979e0f4e33374f319
PVega17102 <is.paulvega.17102@gmail.com>
2023-10-25 20:25:12 -0600

    Home design

--- app/Home.js
+++ app/Home.js
@@ -1,20 +1,76 @@
-import { Button, Image, Text, View } from 'react-native';
+import { Button, Image, Pressable, ScrollView, Text, View } from 'react-native';
 
-import styles from './styles/start.module.css'
+import styles from './styles/home.module.css'
 
 const Home = ({ navigation }) => {
   return (
     <View style={styles.container}>
       {/* <Image style={styles.logo} source={require('./img/netShield.png')}/> */}
       <Text style={styles.title}>Home</Text>
-      <View style={styles.content}>
-        <Text style={styles.description}>Tu entrada a la ciberseguridad. Aprende y protege datos con una experiencia flexible para principiantes y profesionales.</Text>
+      <View style={styles.videocard}>
+
       </View>
-      <View style={styles.button}>
-        <Button title='COMENZAR' color={'#5D92B0'} onPress={() => navigation.navigate('Start')}/>
-      </View>
+      <ScrollView style={styles.scrollY}>
+        <View style={styles.section}>
+          <Text style={styles.course}>Cursos</Text>
+          <ScrollView horizontal={true} style={styles.scrollX}>
+            {courseList.map((courseInfo, index)=>
+            <Pressable key={index} onPress={()=>{console.log(courseInfo.name)}}>
+              <View style={styles.card}>
+                <View style={styles.imgcontainer}>
+                  <Image source={courseInfo.img} style={styles.courseimage}/>
+                </View>
+                <Text style={styles.name}>{courseInfo.name}</Text>
+              </View>
+            </Pressable>
+            )}
+          </ScrollView>
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.course}>Tendencia</Text>
+          <ScrollView horizontal={true} style={styles.scrollX}>
+            {courseList.map((courseInfo, index)=>
+            <Pressable key={index} onPress={()=>{console.log(courseInfo.name)}}>
+              <View style={styles.card}>
+                <View style={styles.imgcontainer}>
+                  <Image source={courseInfo.img} style={styles.courseimage}/>
+                </View>
+                <Text style={styles.name}>{courseInfo.name}</Text>
+              </View>
+            </Pressable>
+            )}
+          </ScrollView>
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
 export default Home
+
+const courseList = [
+  {
+    name: 'Actualizaciones regulares de software',
+    img: require('./img/update.png'),
+  },
+  {
+    name: 'Online scamming',
+    img: require('./img/scamming.png'),
+  },
+  {
+    name: 'Phishing',
+    img: require('./img/phishing.png'),
+  },
+  {
+    name: 'Protección de identidad personal',
+    img: require('./img/personal-identity-protection.png'),
+  },
+  {
+    name: 'Contraseñas fuertes',
+    img: require('./img/password.png'),
+  },
+  {
+    name: 'Protección contra ciberataques',
+    img: require('./img/hackers.png'),
+  },
+]
